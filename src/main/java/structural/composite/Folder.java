21b4aa7858cde8565bb4921d88655042cf3d0845
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-05-25 18:26:41 +0200

    composite in progress

--- src/main/java/structural/composite/Folder.java
+++ src/main/java/structural/composite/Folder.java
@@ -29,11 +29,18 @@ public class Folder implements Component{
 
     @Override
     public Optional<Component> findComponentByName(String nameFragment) {
-
-
-
-
-
+        //czy ja się tak nazywam?
+        if (name.contains(nameFragment)) {
+            return Optional.of(this);
+        }
+        //czy ktoś w środku się tak nazywa?
+        for (Component component : components) {
+            Optional<Component> innerOptional = component.findComponentByName(nameFragment);
+            if (innerOptional.isPresent()) { // czy jest coś w środku?
+                return innerOptional; // tak! zwracam
+            }
+        }
+        return Optional.empty(); // nie, pusty
     }
 
     public void add(Component component){
